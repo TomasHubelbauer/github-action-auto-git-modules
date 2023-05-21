2e8854670f85f8b1fb3d44b7912483b72f30c8d2
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-05-21 20:38:05 +0200

    Make the string to replace optional
    Titles and descriptions can be empty, it is up to the calling workflow to check for this.

--- index.js
+++ index.js
@@ -122,8 +122,8 @@ for (const dotGitmodule of dotGitmodules) {
 // Note that this is probably incorrect and Node doesn't ship an API for this
 // because "this seems like a userland thing hurr durr" ugh
 // See https://github.com/nodejs/node/issues/34840
-function escapeShell(string) {
-  return '"' + string.replace(/(["'$`\\])/g, '\\$1') + '"';
+function escapeShell(/** @type {string | undefined} */ string) {
+  return '"' + string?.replace(/(["'$`\\])/g, '\\$1') + '"';
 }
 
 // TODO: Also fetch the README and add a `title` to the `.gitmodules` entry
